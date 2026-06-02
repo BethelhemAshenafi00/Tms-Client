7f8eb7ec5e35f387fe3fe194a8f7fd52cb6b2fc5
ermi-e3 <ermiyas_eshetu@outlook.com>
2026-06-02 10:42:57 +0300

    test conflict

--- assessment-config.ts
+++ assessment-config.ts
@@ -1,1 +1,5 @@
 "export const PASS_THRESHOLD = 50;";
+"export const PASS_THRESHOLD = 60;";
+"export const PASS_THRESHOLD = 70;";
+"export const PASS_THRESHOLD = 80;";
+"export const PASS_THRESHOLD = 90;";
